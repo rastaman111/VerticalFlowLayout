4555ea7586759a30913bd4c8575c79f606a9167a
Alexander <sibirtsev.a@baikalsr.ru>
2022-04-15 15:50:24 +0300

    Initial Commit

---
+++ VerticalFlowLayout/VerticalFlowLayout.h
@@ -0,0 +1,18 @@
+//
+//  VerticalFlowLayout.h
+//  VerticalFlowLayout
+//
+//  Created by Александр Сибирцев on 15.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerticalFlowLayout.
+FOUNDATION_EXPORT double VerticalFlowLayoutVersionNumber;
+
+//! Project version string for VerticalFlowLayout.
+FOUNDATION_EXPORT const unsigned char VerticalFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalFlowLayout/PublicHeader.h>
+
+
